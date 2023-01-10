987a5ea1be342fa246cd5fdf55b40dec04c5e893
prestonf2003 <prestonfernando@gmail.com>
2023-01-09 20:01:14 -0500

    Added More Pages

---
+++ src/components/Courses/CoursesPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class CoursePage extends React.Component {
+  render() {
+    return <h2>Courses</h2>;
+  }
+}
+export default CoursePage;
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageNotFound = () => <h1>This Page is not Found</h1>;
+
+export default PageNotFound;
